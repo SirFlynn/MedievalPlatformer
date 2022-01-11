ea83733569c781ffea1b14b150db6925e072c4ed
Sir Flynn <flynnvanstryp@gmai.com>
2022-01-11 22:51:46 +1100

    Added quick scripts and started on programming player using platforms

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,24 @@ public class Player : MonoBehaviour
 {
     public CharacterController2D controller;
 
+    public GameObject platform;
+    //public GameObject clonePlatform;
+    // List<GameObject> unityGameObjects = new List<GameObjects>();
+
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
 
-    void Update ()
+    //Creating the platform
+    public void Spawn(Vector3 position)
+    {
+        //creates the platform at the position and destroys old platform
+        GameObject clonePlatform = Instantiate(platform).transform.position = position;
+        clonePlatform.tag = "Clone";
+    }
+
+    void Update()
     {
         //if A or left arrow pressed this moves the player
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -23,10 +35,27 @@ public class Player : MonoBehaviour
         }
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         //Move the player, allows player to jump
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
+
+        //if F key pressed spawn object at mousepointer
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //destroy clone
+            Destroy(platform);
+
+            //Takes the mouse position (on the canvas) and converts them into the transform co-ordinates in game
+            Vector3 worldpoint = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+
+            //Make Z position the Z position of the platform prefab not the camera
+            Vector3 adjustZ = new Vector3(worldpoint.x, worldpoint.y, platform.transform.position.z);
+            Spawn(adjustZ);
+
+            //adds platform to list
+            //unityGameObjects.Add(platform);
+        }
     }
 }
